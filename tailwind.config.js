9481be588ae240567b3f51a76c0908129bf84a49
김보민 <104645059+bominkim90@users.noreply.github.com>
2026-01-10 18:48:04 +0900

    feat: font 적용 & color 토큰 설계
    ## 작업 내용
    - font : next/font + tailwind
    - color: :root 변수 + tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+      },
+      colors: {
+        primitive: {
+          white: 'rgb(var(--color-white) / <alpha-value>)',
+          blackPrimary: 'rgb(var(--color-black-primary) / <alpha-value>)',
+          blackSecond: 'rgb(var(--color-black-second) / <alpha-value>)',
+          blackThird: 'rgb(var(--color-black-third) / <alpha-value>)',
+          green: 'rgb(var(--color-green) / <alpha-value>)',
+          grayPrimary: 'rgb(var(--color-gray-primary) / <alpha-value>)',
+          graySecond: 'rgb(var(--color-gray-second) / <alpha-value>)',
+          grayText: 'rgb(var(--color-gray-text) / <alpha-value>)',
+        },
+        bg: {
+          primary: 'rgb(var(--color-bg-primary) / <alpha-value>)',
+        },
+        text: {
+          primary: 'rgb(var(--color-text-primary) / <alpha-value>)',
+          second: 'rgb(var(--color-text-second) / <alpha-value>)',
+          third: 'rgb(var(--color-text-third) / <alpha-value>)',
+        },
+        button: {
+          bg: {
+            primary: 'rgb(var(--color-button-bg-primary) / <alpha-value>)',
+            second: 'rgb(var(--color-button-bg-second) / <alpha-value>)',
+          },
+        },
+      },
+    },
+  },
 };
